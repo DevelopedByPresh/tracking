835aa1f917fc10df7dc42c1eb8662ec01c1ddf25
Henry <klassicwhyte@gmail.com>
2025-11-13 15:24:58 +0100

    few changes

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var val = document.querySelector('.val')
 
 function Redirect(){
 
-  if(track_no.value === 'CS-568894678'){
+  if(track_no.value === 'CS-568271835'){
    setTimeout(function () { window.location = "myTracking.html" }, 1);
   }
   
