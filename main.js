ac9af771d86cb89a2c622ca6470dbbe27f223035
Henry <klassicwhyte@gmail.com>
2025-12-22 09:30:33 +0100

    few changes

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function Redirect(){
     setTimeout(function () { window.location = "parcel.html" }, 1);
    }
 
-   else if(track_no.value === 'CS-5678995788'){
+   else if(track_no.value === 'CS-5678995789'){
     setTimeout(function () { window.location = "goods.html" }, );
    }
 
